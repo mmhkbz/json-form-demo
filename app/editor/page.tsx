10ab8cb57ecfe815d62d797558c8c5533c0ccaf3
mmhkbz <myomin.htoo@kbzbank.com>
2025-12-10 03:21:25 +0000

    feat: editor page

---
+++ app/editor/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function EditorPage(){
+    return (
+        <div className="w-screen h-screen">
+            <iframe src="https://jsonformseditor.io/" className="w-full h-full"></iframe>
+        </div>
+    )
+}
